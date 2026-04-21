2ddfe1a0b7dec015892e8fae0d58bb0faedf1a33
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-04-21 12:14:22 -0400

    Update play_predictor.py

--- play_predictor.py
+++ play_predictor.py
@@ -63,11 +63,11 @@ def play_proba():
     return
  
   
-  X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
   scaler = StandardScaler()
   scaler.fit(X_train)
-  X_train_scaled = scaler.transform(X)
+  X_train_scaled = scaler.transform(X_train)
   
   dtc_re2 = RandomForestClassifier(max_depth=13, criterion='entropy', class_weight='balanced', n_estimators=100, random_state=42)
   dtc_re2.fit(X_train, y_train.values.ravel())
