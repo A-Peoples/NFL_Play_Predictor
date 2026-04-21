79f59d973793b8393eaa7b82a12ba035e6f9e51e
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-04-21 11:22:59 -0400

    Update play_predictor.py

--- play_predictor.py
+++ play_predictor.py
@@ -65,11 +65,11 @@ X_scaled = scaler.transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 dtc_re2 = RandomForestClassifier(max_depth=13, criterion='entropy', class_weight='balanced', n_estimators=100, random_state=42)
-dtc_re2.fit(X_train, y_train)
+dtc_re2.fit(X_train, y_train.values.ravel())
 
 user_checks = [[home_team_num, away_team_num, down, ydstogo, pd, yt_ez, game_sec]]
 user_checks_scaled = scaler.transform(user_checks)
 prob = dtc_re2.predict_proba(user_checks_scaled)
-st.write('Probabilities: ' + prob[0])
+#st.write('Probabilities: ' + prob[0])
 #st.dataframe(column_dataset, use_container_width=True)
 
