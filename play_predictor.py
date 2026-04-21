de79ff1f270bdb4e43eb5ff33f72c7f56d164580
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-04-21 14:31:16 -0400

    Update play_predictor.py

--- play_predictor.py
+++ play_predictor.py
@@ -73,7 +73,7 @@ def play_proba():
     return
  
   
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
 
   scaler = StandardScaler()
   scaler.fit(X_train)
