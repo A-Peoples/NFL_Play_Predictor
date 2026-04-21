aeeda5f6780c9bfab7f9b07f91125e35bb4d77e0
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-04-21 12:24:42 -0400

    Update play_predictor.py

--- play_predictor.py
+++ play_predictor.py
@@ -48,8 +48,8 @@ with col4:
   ydstogo = st.selectbox("Yards To Go", range(1, 43))
 
 column_dataset = test_dataset.loc[(((test_dataset['posteam_pd'] - p_diff).isin(range(-5, 5))) & 
-                                   ((test_dataset['game_seconds_remaining'] - game_sec).isin(range(-30, 31)) & (test_dataset['down'] == down) & 
-                                   (test_dataset['yardline_100'] - yt_ez).isin(range(-5, 5))))].reset_index(drop=True)
+                                   ((test_dataset['game_seconds_remaining'] - game_sec).isin(range(-60, 61)) & (test_dataset['down'] == down) & 
+                                   (test_dataset['yardline_100'] - yt_ez).isin(range(-10, 10))))].reset_index(drop=True)
 
 
 features = ['posteam_num', 'defteam_num', 'down', 'ydstogo', 'posteam_pd','yardline_100', 'game_seconds_remaining']
