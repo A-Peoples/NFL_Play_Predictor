605e65e95e21c7b956ff8d5e7abd030d2cbffd68
Ah-Lon Peoples, II <143513372+A-Peoples@users.noreply.github.com>
2026-04-21 09:44:13 -0400

    Update play_predictor.py

--- play_predictor.py
+++ play_predictor.py
@@ -31,7 +31,7 @@ with col1:
 with col2:
   quarter = st.selectbox("Quarter", range(1, 5))
   minutes = st.slider("Minutes in Quarter", 0, 15, 15)
-  seconds = st.slider("Seconds in Quarter", 0, 60, 0)
+  seconds = st.slider("Seconds in Quarter", 0, 59, 0)
   if minutes == 15:
     seconds = 0
   game_sec = (quarter * 900) - (900 - (minutes*60 + seconds))
